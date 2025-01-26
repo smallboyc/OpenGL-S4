04b7d522341ff3797b63f6a369f9d9129d80d586
smallboyc <maxencedup44@gmail.com>
2025-01-26 11:59:42 +0100

    :sparkles: Basic tranformations

--- TP2/shaders/color2D.vs.glsl
+++ TP2/shaders/color2D.vs.glsl
@@ -5,7 +5,22 @@ layout(location = 1) in vec3 aVertexColor;
 
 out vec3 vFragColor;
 
+mat3 translate(float tx, float ty) {
+    return mat3(vec3(1, 0, 0), vec3(0, 1, 0), vec3(tx, ty, 1));
+}
+
+mat3 scale(float sx, float sy) {
+    return mat3(vec3(sx, 0, 0), vec3(0, sy, 0), vec3(0, 0, 1));
+}
+
+mat3 rotate(float angle_deg) {
+    float angle_rad = radians(angle_deg);
+    return mat3(vec3(cos(angle_rad), sin(angle_rad), 0), vec3(-sin(angle_rad), cos(angle_rad), 0), vec3(0, 0, 1));
+}
+
 void main() {
     vFragColor = aVertexColor;
-    gl_Position = vec4(aVertexPosition, 0, 1);
+    // vec2 eVertexPosition = (translate(0.5, 0.5) * vec3(aVertexPosition, 1)).xy;
+    vec2 eVertexPosition = (translate(0.5, 0.5) * vec3(aVertexPosition, 1)).xy;
+    gl_Position = vec4(eVertexPosition, 0, 1);
 }
